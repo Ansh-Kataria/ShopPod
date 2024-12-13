212fcad485aa034b03b5f0e66d451185d0d3259a
Ansh-Kataria <anshkataria28@gmail.com>
2024-12-13 16:23:19 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-500 text-white">
+      <h1 className="text-3xl font-bold">Hello, Tailwind CSS with Vite!</h1>
+    </div>
+  );
+}
+
+export default App;
